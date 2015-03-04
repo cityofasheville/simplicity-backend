66b6f3b294dbc62a6d6d763b3573535efcbde6ef
Dave Michelson <daveism@gmail.com>
2015-03-04 09:06:13 -0500

    adjust re-indexing
    re-index was completed before data was inserted.  added a slight delay
    to start

--- create-overlays.sh
+++ create-overlays.sh
@@ -11,7 +11,7 @@ sleep 2m
 /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/truncate_cache.sql
 /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/reindex-shapes.sql
 rcraw=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/address_count.sql -t)
-START=0 
+START=0
 inc=1000
 i=$START
 END=$((rcraw+0))
@@ -21,40 +21,40 @@ while [ $i -le $END ]
 do
     ((n=$i+$inc))
     layerids=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_id.sql -t)
-    for layerid in $layerids 
+    for layerid in $layerids
     do
       layername=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_name.sql -t -v one=$layerid)
       shopt -s extglob
       layername=${layername##*( )}
       shopt -u extglob
 
-      layerdist=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_distance.sql -t -v one=$layerid)  
+      layerdist=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_distance.sql -t -v one=$layerid)
       shopt -s extglob
       layerdist=${layerdist##*( )}
       shopt -u extglob
 
-      layersql=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_sql.sql -t -v one=$layerid)  
+      layersql=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/layer_sql.sql -t -v one=$layerid)
       shopt -s extglob
       layersql=${layersql##*( )}
       shopt -u extglob
 
       if [  $((layerdist+0)) -eq 1 ]
       then
-        distances=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/distances.sql -t)         
+        distances=$(psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/distances.sql -t)
         for dist in $distances
         do
           shopt -s extglob
           dist=${dist##*( )}
           shopt -u extglob
           echo "Processing records $i - $n using $layername at a distance of $dist"
-          /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/$layersql  -v overlaytype="'$layername'" -v distance=$dist -v record=$i -v offset=$inc & 
+          /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/$layersql  -v overlaytype="'$layername'" -v distance=$dist -v record=$i -v offset=$inc &
         done
       else
          echo "Processing records $i - $n using $layername"
          /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/$layersql  -v overlaytype="'$layername'" -v distance=0 -v record=$i -v offset=$inc &
       fi
     done
-    echo $(echo "scale=2; ($i/$END)*100" | /usr/bin/bc)" % complete" 
+    echo $(echo "scale=2; ($i/$END)*100" | /usr/bin/bc)" % complete"
     ((i = i + $inc))
     sleep 3
 done
@@ -64,4 +64,5 @@ sleep 600
 echo $(date)
 /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/update_coa_street_name_cache.sql &
 /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/update-live.sql &
+sleep 600
 /usr/bin/psql -U [CHANGE-TO-YOUR-POSTGRES-USERNAME] -d coagis -f /home/ubuntu/job/reindex-shapes.sql
