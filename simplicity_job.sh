ffc97f649a9e8af98b5d96edb64515c46a16b1b3
Dave Michelson <daveism@gmail.com>
2015-04-27 09:05:34 -0400

    added test for all

--- simplicity_job.sh
+++ simplicity_job.sh
@@ -4,20 +4,8 @@ export date=`date +%d-%m-%Y`
 node simplicity-backend.js -d config/db.yml -m config/maintenance.yml >> log/maintenance_$date.log
 node simplicity-backend.js -d config/db.yml -c config/buildcache.yml >> log/buildcache_$date.log
 node simplicity-backend.js -d config/db.yml -m config/maintenance_afterbuild.yml >> log/maintenance_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_address.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_buffer.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_cache.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_citylimits.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_crime.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_crossref.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_neighborhoods.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_permits.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_property.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_sanitationdistricts.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_streetazimuthal.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_streets.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_zoning.yml >> log/datatest_$date.log
-node simplicity-backend.js -d config/db.yml -t config/datatests_zoningoverlays.yml >> log/datatest_$date.log
+node simplicity-backend.js -d config/db.yml -t config/datatests_all.yml>> log/datatest_$date.log
+
 
 #remove files over a week old.
 find /home/ubuntu/simplicity-backend/log/*.log -mtime +14  -exec rm {} \;
